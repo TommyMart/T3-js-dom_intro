9a3290d93b008d7a7f251c8d26c9af127ea02e08
Thomas Martin <161907136+TommyMart@users.noreply.github.com>
2024-08-27 19:49:02 +0930

    feat: remove button now exists, no functionality

--- src/scripts/dataList.js
+++ src/scripts/dataList.js
@@ -1,3 +1,5 @@
+
+// Declare data to use in the logic
 let dataArray = [
     "techno",
     "house",
@@ -7,23 +9,36 @@ let dataArray = [
     "soul"
 ]
 
+// Find elements on the page for us to use in the logic
 let genreContainerElement = document.getElementById("genreContainer");
 
-
+// Create elements to add to the page via the logic
 let genresContainerList = document.createElement("ul");
 
+// Do logic for each individual item in the array
 dataArray.forEach( (musicGenre) => {
     console.log(musicGenre);
 
+    // Verify what we are working with, just log it to see the data's value
     newGenreEntry = document.createElement("li");
 
+    // Create new HTML element to help us format the data's value
     newGenreEntry.innerText += musicGenre;
 
+    // add a button to each entry
+    // click on the button to remove the entry from the list
+    // let removeButton = docutmentElement("button"); same as below
+    removeButton.innerText = `Remove ${musicGenre}`;
+    // Add the removeButton to the genre entry
+    newGenreEntry.appendChild(removeButton);
+
+    // add the nicely formatted element into the list container
     genresContainerList.appendChild(newGenreEntry);
 
 
 });
 
+// Add the list container onto the actual HTML page
 genreContainerElement.appendChild(genresContainerList);
 
 
