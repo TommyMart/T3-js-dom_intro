06253d01ca9b690b7244cd6289510e1ee56d88fa
Thomas Martin <161907136+TommyMart@users.noreply.github.com>
2024-08-27 19:25:32 +0930

    feat: populate page w content

--- src/scripts/dataList.js
+++ src/scripts/dataList.js
@@ -3,10 +3,29 @@ let dataArray = [
     "house",
     "disco",
     "ambient",
-    "funk"
+    "funk", 
+    "soul"
 ]
+
+let genreContainerElement = document.getElementById("genreContainer");
+
+
+let genresContainerList = document.createElement("ul");
 
 dataArray.forEach( (musicGenre) => {
     console.log(musicGenre);
+
+    newGenreEntry = document.createElement("li");
+
+    newGenreEntry.innerText += musicGenre;
+
+    genresContainerList.appendChild(newGenreEntry);
+
+
 });
+
+genreContainerElement.appendChild(genresContainerList);
+
+
+
 
