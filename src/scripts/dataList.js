f44093e26dbeeae9e0a33f0091891d1c823db73c
Thomas Martin <161907136+TommyMart@users.noreply.github.com>
2024-08-29 20:06:17 +0930

    feat: form input

--- src/scripts/dataList.js
+++ src/scripts/dataList.js
@@ -37,7 +37,7 @@ function renderData(){
         let newGenreEntry = document.createElement("li");
 
         // Create new HTML element to help us format the data's value
-        newGenreEntry.innerText += (musicGenre + " \n");
+        newGenreEntry.innerText += musicGenre;
 
         // Add a button to each entry
         // click on the button to remove the entry from the list
@@ -70,4 +70,49 @@ function removeGenreFromDataList (targetItemToRemove) {
     renderData();
 }
 
+
+function addGenreToDataList(event, targetInputId){
+
+    // Find the form element
+    let formElement = document.getElementById("genresInputForm");
+    // Use the form element.checkValidity() method and save the results
+    let isFormValid = formElement.checkValidity();
+    console.log("isFormValid value: " + isFormValid);
+
+    // do a conditional based on that result value
+    if (!isFormValid){
+        formElement.reportValidity();
+        return;
+    } 
+
+
+    // Find the form from the event
+    // Prevent the form from doing its default behaviour (refreshing page)
+    event.preventDefault();
+    console.log("Add car to list function is now running!");
+
+    // Find the input text field based on targetInputId
+    // Grab the string value from the text field
+    let targetTextInput = document.getElementById(targetInputId);
+    // Push the string value into dataArray
+    console.log(targetTextInput.value);
+
+    // Push the string value into dataArray
+	dataArray.push(targetTextInput.value);
+
+	// Clear out the input field text to be blank again 
+	targetTextInput.value = "";
+
+	// Focus on the text input field again to enable quick data entry!
+	targetTextInput.focus();
+    
+    alert("Submitted a new entry: " + dataArray[dataArray.length -1]);
+
+	// call renderData() to update the page 
+	renderData();
+    
+}
+
+let formInputButton = document.getElementById("formInputButton");
+formInputButton.addEventListener("click", (event) => addGenreToDataList(event, "genreInputText"))
 
